a2d0ffefd2a2bd97d5753ea488e182cf41b31e81
n0Oo0Oo0b <bobthebuilder10492@gmail.com>
2023-03-08 13:19:39 +0800

    Convert int/float literals during parsing (instead of runtime)

--- cambridgeScript/tokens.py
+++ cambridgeScript/tokens.py
@@ -14,14 +14,12 @@ Value = str | int | float | bool
 class Token:
     type: str
     value: Value | str | None
+    meta: str | None = None
 
     def resolve(self, variables: VariableState | None = None) -> Value:
         variables = variables or VariableState.default_state
         if self.type == 'VALUE':
-            if '.' in self.value:
-                return float(self.value)
-            else:
-                return int(self.value)
+            return self.value
         elif self.type == 'IDENTIFIER':
             return variables[self.value]
         else:
@@ -39,11 +37,18 @@ def parse_tokens(code: str) -> list[Token]:
     code += '\n'
     res: list[Token] = []
     for match in re.finditer(TOKEN_REGEX, code, re.M):
-        if match.lastgroup == 'IGNORE' or match.lastgroup == 'COMMENT':
+        token_type = match.lastgroup
+        token_value = str(match.group())
+        if token_type == 'IGNORE' or token_type == 'COMMENT':
             continue
-        if match.lastgroup == 'IDENTIFIER' and match.group() in KEYWORDS:
-            token = Token(str(match.group()), None)
+        elif token_type == 'IDENTIFIER' and token_value in KEYWORDS:
+            token = Token(token_value, None)
+        elif token_type == 'VALUE':
+            if '.' in token_value:
+                token = Token(token_type, float(token_value), 'REAL')
+            else:
+                token = Token(token_type, int(token_value), 'INTEGER')
         else:
-            token = Token(match.lastgroup, str(match.group()))
+            token = Token(token_type, token_value)
         res.append(token)
     return res
