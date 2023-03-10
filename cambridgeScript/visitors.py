9de14f1eb05c00b028102fef6ed9de4f015d0e85
n0Oo0Oo0b <bobthebuilder10492@gmail.com>
2023-03-10 15:09:17 +0800

    `visit_unary_op` visitor function and typehint improvements

--- cambridgeScript/visitors.py
+++ cambridgeScript/visitors.py
@@ -1,23 +1,40 @@
 from abc import ABC, abstractmethod
-from typing import TYPE_CHECKING
+from typing import Any, TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from parser.syntax_tree import Primary, BinaryOp, FunctionCall
+    from parser.syntax_tree import Primary, UnaryOp, BinaryOp, FunctionCall
+
+
+Value = str | int | float | bool
 
 
 class ExpressionVisitor(ABC):
     @abstractmethod
-    def visit_primary(self, expr: 'Primary'):
+    def visit_primary(self, expr: 'Primary') -> Any:
         pass
 
     @abstractmethod
-    def visit_binary_op(self, expr: 'BinaryOp'):
+    def visit_unary_op(self, expr: 'UnaryOp') -> Any:
         pass
 
-    # @abstractmethod
-    # def visit_unary_op(self, expr: 'UnaryOp'):
-    #     pass
+    @abstractmethod
+    def visit_binary_op(self, expr: 'BinaryOp') -> Any:
+        pass
 
     @abstractmethod
-    def visit_function_call(self, expr: 'FunctionCall'):
+    def visit_function_call(self, expr: 'FunctionCall') -> Any:
+        pass
+
+
+class ExpressionResolver(ExpressionVisitor):
+    def visit_primary(self, expr: 'Primary') -> Value:
+        pass
+
+    def visit_unary_op(self, expr: 'UnaryOp') -> Value:
+        pass
+
+    def visit_binary_op(self, expr: 'BinaryOp') -> Value:
+        pass
+
+    def visit_function_call(self, expr: 'FunctionCall') -> Value:
         pass
