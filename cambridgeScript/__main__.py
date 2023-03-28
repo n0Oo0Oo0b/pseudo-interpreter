b78f2a0a66751558eaca03d829edad069a6ffb41
Silkyway <ethancai920@gmail.com>
2023-03-28 10:48:21 +0800

    bad cli

--- cambridgeScript/__main__.py
+++ cambridgeScript/__main__.py
@@ -1,7 +1,27 @@
 from cambridgeScript.interpreter.programs import Program
+import argparse
+
 
 
 if __name__ == "__main__":
-    p = Program.from_code(open(0).read())
-    print(p)
-    p.execute()
+    # p = Program.from_code(open(0).read())
+    # print(p)
+    # p.execute()
+
+    parser = argparse.ArgumentParser(description='Cambridge Script CLI')
+    subparsers = parser.add_subparsers(help='sub-command help')
+    run_parser = subparsers.add_parser("run", help="run a program")
+    run_parser.add_argument("filename", type=str, help="name of the file")
+    
+    def run(args):
+        with open(args.filename) as f:
+            p = Program.from_code(f.read())
+            print(p)
+            p.execute()
+
+    run_parser.set_defaults(func=run)
+    args = parser.parse_args()
+    
+    args.func(args)
+    
+
