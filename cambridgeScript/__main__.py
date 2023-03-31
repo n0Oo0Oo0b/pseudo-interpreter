9e87822e8ce3e686b7f093031d7caee43add69c0
n0Oo0Oo0b <bobthebuilder10492@gmail.com>
2023-03-31 12:21:56 +0800

    Temporary main file to test statement parsing

--- cambridgeScript/__main__.py
+++ cambridgeScript/__main__.py
@@ -1,7 +1,9 @@
-from .interpreter.programs import Program
-
+from .parser.parser import Parser
+from .parser.tokens import parse_tokens
 
 if __name__ == "__main__":
-    p = Program.from_code(open(0).read())
-    print(p)
-    p.execute()
+    tokens = parse_tokens(open(0).read())
+    for token in tokens:
+        print(token)
+    parser = Parser(tokens)
+    print(parser.block())
