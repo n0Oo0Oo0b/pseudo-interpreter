dc97a9ff619e94e73e28403f8a8d4b9f4719470e
n0Oo0Oo0b <bobthebuilder10492@gmail.com>
2023-05-16 12:28:31 +0800

    Test if tokens work correctly in __main__.py

--- cambridgeScript/__main__.py
+++ cambridgeScript/__main__.py
@@ -1,25 +1,30 @@
-import click
-
-from .interpreter.programs import Program
-
-
-@click.group(invoke_without_command=True)
-@click.pass_context
-def cli(ctx: click.Context):
-    if ctx.invoked_subcommand:
-        return
-
-    click.echo("REPL is under construction")
-
-
-@cli.command()
-@click.argument("file", type=click.File())
-def run(file):
-    code = file.read()
-    code += "\n"
-    program = Program.from_code(code)
-    program.execute()
+# import click
+#
+# from .interpreter.programs import Program
+#
+#
+# @click.group(invoke_without_command=True)
+# @click.pass_context
+# def cli(ctx: click.Context):
+#     if ctx.invoked_subcommand:
+#         return
+#
+#     click.echo("REPL is under construction")
+#
+#
+# @cli.command()
+# @click.argument("file", type=click.File())
+# def run(file):
+#     code = file.read()
+#     code += "\n"
+#     program = Program.from_code(code)
+#     program.execute()
 
 
 if __name__ == "__main__":
-    cli()
+    # cli()
+    from .parser.tokens import parse_tokens
+
+    tokens = parse_tokens(open(0).read())
+    for token in tokens:
+        print(token)
