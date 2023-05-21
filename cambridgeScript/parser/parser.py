b689df73fe876adabdd7536c9ba89d4874a4a4e9
n0Oo0Oo0b <bobthebuilder10492@gmail.com>
2023-05-21 14:36:51 +0800

    Add an offset to peek

--- cambridgeScript/parser/parser.py
+++ cambridgeScript/parser/parser.py
@@ -18,11 +18,14 @@ class Parser:
         """Returns whether the pointer is at the end"""
         return self._next_index == len(self.tokens)
 
-    def _peek(self) -> Token | None:
+    def _peek(self, offset: int = 0) -> Token | None:
         """Returns the next token without consuming"""
         if self._is_at_end():
             return None
-        return self.tokens[self._next_index]
+        target = self._next_index + offset
+        if not 0 <= target < len(self.tokens):
+            return None
+        return self.tokens[target]
 
     def _advance(self) -> Token:
         """Consumes and returns the next token"""
