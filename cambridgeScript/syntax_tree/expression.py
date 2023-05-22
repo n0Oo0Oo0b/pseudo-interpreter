6aff695618da9a84a8e3e53f2640432333d9287b
n0Oo0Oo0b <bobthebuilder10492@gmail.com>
2023-05-22 11:36:11 +0800

    Move assignment to statements

--- cambridgeScript/syntax_tree/expression.py
+++ cambridgeScript/syntax_tree/expression.py
@@ -1,7 +1,6 @@
 __all__ = [
     "ExpressionVisitor",
     "Expression",
-    "Assignment",
     "BinaryOp",
     "UnaryOp",
     "FunctionCall",
--- cambridgeScript/syntax_tree/statement.py
+++ cambridgeScript/syntax_tree/statement.py
@@ -18,6 +18,7 @@ __all__ = [
     "FileWriteStmt",
     "FileCloseStmt",
     "ProcedureCallStmt",
+    "AssignmentStmt",
     "ExprStmt",
 ]
 
