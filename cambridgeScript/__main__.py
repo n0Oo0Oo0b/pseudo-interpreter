3fcdc88edd648bb94bca1c52182d1dd36587ac55
n0Oo0Oo0b <bobthebuilder10492@gmail.com>
2023-06-18 04:40:28 +0800

    update __main__.py

--- cambridgeScript/__main__.py
+++ cambridgeScript/__main__.py
@@ -25,9 +25,13 @@ if __name__ == "__main__":
     # cli()
     from cambridgeScript.parser.lexer import parse_tokens
     from cambridgeScript.parser.parser import Parser
+    from cambridgeScript.interpreter.variables import VariableState
+    from cambridgeScript.interpreter.interpreter import Interpreter
 
     tokens = parse_tokens(open(0).read())
     for token in tokens:
         print(token)
     parsed = Parser.parse_program(tokens)
     print(parsed)
+    interpreter = Interpreter(VariableState())
+    interpreter.visit(parsed)
