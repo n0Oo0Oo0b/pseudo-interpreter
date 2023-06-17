9f4acd16fb80051ddfaaa932d3b4e5e213dfaafb
n0Oo0Oo0b <bobthebuilder10492@gmail.com>
2023-06-18 04:39:21 +0800

    make VariableState fields optional

--- cambridgeScript/interpreter/variables.py
+++ cambridgeScript/interpreter/variables.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 from cambridgeScript.parser.lexer import Value
 from cambridgeScript.syntax_tree import FunctionDecl, ProcedureDecl
@@ -6,7 +6,7 @@ from cambridgeScript.syntax_tree import FunctionDecl, ProcedureDecl
 
 @dataclass
 class VariableState:
-    variables: dict[str, Value | None]
-    constants: dict[str, Value]
-    functions: dict[str, FunctionDecl]
-    procedures: dict[str, ProcedureDecl]
+    variables: dict[str, Value | None] = field(default_factory=dict)
+    constants: dict[str, Value] = field(default_factory=dict)
+    functions: dict[str, FunctionDecl] = field(default_factory=dict)
+    procedures: dict[str, ProcedureDecl] = field(default_factory=dict)
