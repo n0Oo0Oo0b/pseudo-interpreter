cea6cea5766d3c40be0e68cc9800aeab0c34d976
n0Oo0Oo0b <bobthebuilder10492@gmail.com>
2023-06-18 04:22:19 +0800

    remove MissingVaulue
    replaced with None since it's easier to work with

--- cambridgeScript/interpreter/variables.py
+++ cambridgeScript/interpreter/variables.py
@@ -4,16 +4,9 @@ from cambridgeScript.parser.lexer import Value
 from cambridgeScript.syntax_tree import FunctionDecl, ProcedureDecl
 
 
-class _MissingValueSentinel:
-    pass
-
-
-MissingValue = _MissingValueSentinel()
-
-
 @dataclass
 class VariableState:
-    variables: dict[str, Value | _MissingValueSentinel]
+    variables: dict[str, Value | None]
     constants: dict[str, Value]
     functions: dict[str, FunctionDecl]
     procedures: dict[str, ProcedureDecl]
